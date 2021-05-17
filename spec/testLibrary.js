0623b8a8be4bf94854cbbce728e89c94c32760b3
Cynthia Anyaeriuba <canyaeriuba@gmail.com>
2021-05-17 22:12:56 +0100

    added testing library

---
+++ spec/testLibrary.js
@@ -0,0 +1,25 @@
+// helper methods and matchers for testing framework
+"use strict";
+
+function describe(title, body) {
+  console.log(title);
+  body();
+}
+
+function expect(actual) {
+  return {
+    toEqual: function(expected) {
+      if (actual === expected) {
+        console.log("PASS");
+      } else {
+        console.log(`FAIL: expected ${actual} to equal ${expected}`);
+      };
+    }
+  };
+};
+
+function it(label, callback) {
+  console.log(`Test: ${label}`)
+  callback()
+};
+
